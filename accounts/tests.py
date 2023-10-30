13145e0082540570adb80e7d553980bc8fa0a744
Shoorano <danwilloughby2@gmail.com>
2023-10-30 20:34:23 +0000

    added CustomeUser, forms and admin, and built tests for it.

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,33 @@
+# accounts/tests.py
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
-# Create your tests here.
+
+class CustomUserTests(TestCase):
+    def test_create_user(self):
+        """test creation of user"""
+        username = "dan"
+        email = "dan@email.com"
+        password = "testpass123"
+
+        User = get_user_model()
+        user = User.objects.create_user(username=username, email=email, password=password)
+        self.assertEqual(user.username, username)
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+    def test_create_superuser(self):
+        """test creation of superuser"""
+        username = "superuser"
+        email = "admin@email.com"
+        password = "testpass123"
+
+        User = get_user_model()
+        user = User.objects.create_superuser(username=username, email=email, password=password)
+        self.assertEqual(user.username, username)
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.is_active)
+        self.assertTrue(user.is_staff)
+        self.assertTrue(user.is_superuser)
